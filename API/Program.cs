ee3c78ae4fa7e486ca82e72348df93b1a77674c5
Temidayo Titilola Micheal <ttmvalid3110@gmail.com>
2024-03-25 18:10:13 +0000

    Api generic repository

--- API/Program.cs
+++ API/Program.cs
@@ -10,11 +10,16 @@ builder.Services.AddDbContext<StoreContext>(opt => {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped(typeof(IGenericRepository<>),typeof(GenericRepository<>));
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 var app =builder.Build();
 if(app.Environment.IsDevelopment()){
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseStaticFiles();
 app.UseAuthorization();
 app.MapControllers();
 
