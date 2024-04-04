51aa58586cdf146cd43a1a847ce9347800a6c382
Temidayo Titilola Micheal <ttmvalid3110@gmail.com>
2024-04-04 23:47:22 +0100

    Validation _ api

--- API/Helpers/MappingProfiles.cs
+++ API/Helpers/MappingProfiles.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using API.Dtos;
 using AutoMapper;
+using Core.Entities;
 using Core.Entities.Identity;
 using Core.Entitites;
 
@@ -18,6 +19,8 @@ namespace API.Helpers
             .ForMember(d => d.ProductType, o =>o.MapFrom(s=>s.ProductType.Name))
             .ForMember(d =>d.PictureUrl, o=> o.MapFrom<ProductUrlResolver>());
              CreateMap<Address, AddressDto>().ReverseMap();
+              CreateMap<CustomerBasketDto, CustomerBasket>();
+            CreateMap<BasketItemDto, BasketItem>();
         }
     }
 }  
--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b7f3bacac0c15cf77cb5678cbdce690be9f8825")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0de2b5e6748a77beb16952f89e4b034bad262805")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
